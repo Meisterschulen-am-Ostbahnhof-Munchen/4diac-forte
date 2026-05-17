daaae7f72c57f43b0da7b6f324965c066b6d75e5
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2026-05-17 10:23:54 +0200

    Enable additional communication modules and system tests in setup_windows.ps1
    Enhances the Windows setup script to include multiple communication modules such as HTTP, RAW, and several others, alongside system tests and sanitization for improved testing coverage.

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -30,16 +30,28 @@ if (Test-Path $forte_bin_dir) {
     Set-Location $forte_bin_dir
 
     cmake -G "Ninja" `
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON `
+        -DCMAKE_INSTALL_PREFIX="D:/a/4diac-forte/4diac-forte/install/windows-systemtests" `
         -DFORTE_ARCHITECTURE=Win32 `
         -DFORTE_COM_ETH=ON `
         -DFORTE_COM_FBDK=ON `
+        -DFORTE_COM_HTTP=ON `
         -DFORTE_COM_LOCAL=ON `
-        -DFORTE_TESTS=ON `
-        "-DFORTE_TESTS_INC_DIRS=$forte_boost_test_inc_dirs" `
-        "-DFORTE_TESTS_LINK_DIRS=$forte_boost_test_inc_dirs" `
+        -DFORTE_COM_RAW=ON `
+        -DFORTE_COM_SER=ON `
+        -DFORTE_COM_STRUCT_MEMBER=ON `
+        -DFORTE_IO=ON `
         -DFORTE_MODULE_CONVERT=ON `
         -DFORTE_MODULE_IEC61131=ON `
+        -DFORTE_MODULE_RECONFIGURATION=ON `
+        -DFORTE_MODULE_RT_Events=ON `
+        -DFORTE_MODULE_SIGNALPROCESSING=ON `
         -DFORTE_MODULE_UTILS=ON `
+        -DFORTE_SYSTEM_TESTS=ON `
+        -DFORTE_TESTS=ON `
+        -DFORTE_TEST_SANITIZE=ON `
+        "-DFORTE_TESTS_INC_DIRS=$forte_boost_test_inc_dirs" `
+        "-DFORTE_TESTS_LINK_DIRS=$forte_boost_test_inc_dirs" `
         ../../
     
     Set-Location ../../
