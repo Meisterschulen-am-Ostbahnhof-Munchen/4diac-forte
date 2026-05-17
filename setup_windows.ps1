1252103744a19172a96fbe5f1c65aaa921b049d6
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2026-05-17 10:11:34 +0200

    Update Boost paths in setup_windows.ps1 for correct inclusion
    Adjusts the script to specify the correct include directory for Boost within the native library path, ensuring proper setup for CMake configurations on Windows. This change aids in the correct usage of Boost during the building process.

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -12,18 +12,20 @@ Write-Host "--------------------------------------------------------------------
 $forte_bin_dir = "bin/windows"
 
 #set to boost-include directory
-export forte_boost_test_inc_dirs="$(Get-Location)\external\boost.1.87.0"
+$forte_boost_test_inc_dirs = "$(Get-Location)\external\boost.1.87.0\lib\native\include"
 #set to boost-library directory
-export forte_boost_test_lib_dirs="$(Get-Location)\external\boost.1.87.0"
+$forte_boost_test_lib_dirs = "$(Get-Location)\external\boost.1.87.0\lib\native\include"
 
 if (!(Test-Path $forte_bin_dir)) {
     New-Item -ItemType Directory -Path $forte_bin_dir | Out-Null
 }
 
 if (Test-Path $forte_bin_dir) {
+
     Write-Host "For building forte go to $forte_bin_dir and execute 'ninja'"
     Write-Host "forte can be found at $forte_bin_dir/src"
     Write-Host "forte_tests can be found at $forte_bin_dir/tests"
+
 
     Set-Location $forte_bin_dir
 
@@ -33,8 +35,8 @@ if (Test-Path $forte_bin_dir) {
         -DFORTE_COM_FBDK=ON `
         -DFORTE_COM_LOCAL=ON `
         -DFORTE_TESTS=ON `
-        -DFORTE_TESTS_INC_DIRS=${forte_boost_test_inc_dirs} `
-        -DFORTE_TESTS_LINK_DIRS=${forte_boost_test_inc_dirs} `
+        "-DFORTE_TESTS_INC_DIRS=$forte_boost_test_inc_dirs" `
+        "-DFORTE_TESTS_LINK_DIRS=$forte_boost_test_inc_dirs" `
         -DFORTE_MODULE_CONVERT=ON `
         -DFORTE_MODULE_IEC61131=ON `
         -DFORTE_MODULE_UTILS=ON `
