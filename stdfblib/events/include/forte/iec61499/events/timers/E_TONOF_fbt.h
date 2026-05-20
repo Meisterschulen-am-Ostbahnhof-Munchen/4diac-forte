c5b2ecf163143827ebef46db651ee67ca70e3195
Franz Höpfinger <mail@franzhoepfinger.de>
2026-05-20 10:42:07 +0200

    Modify E_TONOF network structure and refactor events
    https://github.com/franz-hoepfinger-4diac/4diac-ide/pull/4
    https://github.com/franz-hoepfinger-4diac/4diac-forte/pull/1
    https://github.com/eclipse-4diac/4diac-ide/pull/2438

--- stdfblib/events/include/forte/iec61499/events/timers/E_TONOF_fbt.h
+++ stdfblib/events/include/forte/iec61499/events/timers/E_TONOF_fbt.h
@@ -1,11 +1,4 @@
 /*************************************************************************
- *** Copyright (c) 2024 HR Agrartechnik GmbH
- *** This program and the accompanying materials are made available under the
- *** terms of the Eclipse Public License 2.0 which is available at
- *** http://www.eclipse.org/legal/epl-2.0.
- ***
- *** SPDX-License-Identifier: EPL-2.0
- ***
  *** FORTE Library Element
  ***
  *** This file was generated using the 4DIAC FORTE Export Filter 3.1.100.202604172003!
--- stdfblib/events/src/timers/E_TONOF_fbt.cpp
+++ stdfblib/events/src/timers/E_TONOF_fbt.cpp
@@ -1,4 +1,11 @@
 /*************************************************************************
+ *** Copyright (c) 2024 HR Agrartechnik GmbH
+ *** This program and the accompanying materials are made available under the
+ *** terms of the Eclipse Public License 2.0 which is available at
+ *** http://www.eclipse.org/legal/epl-2.0.
+ ***
+ *** SPDX-License-Identifier: EPL-2.0
+ ***
  *** FORTE Library Element
  ***
  *** This file was generated using the 4DIAC FORTE Export Filter 3.1.100.202604172003!
@@ -51,6 +58,7 @@ namespace forte::iec61499::events::timers {
         {"E_RF_TRIG"_STRID, "EF"_STRID, "E_DELAY_ON"_STRID, "STOP"_STRID},
         {"E_RF_TRIG"_STRID, "ER"_STRID, "E_DELAY_OFF"_STRID, "STOP"_STRID},
         {"E_RF_TRIG"_STRID, "EF"_STRID, "E_DELAY_OFF"_STRID, "START"_STRID},
+        {{}, "R"_STRID, "E_DELAY_ON"_STRID, "STOP"_STRID},
     });
 
     const auto cDataConnections = std::to_array<SCFB_FBConnectionData>({
