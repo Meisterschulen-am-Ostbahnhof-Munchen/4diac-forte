99eda79e4b291b76a0eae7622a2eae32f1df077e
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2026-05-31 17:55:56 +0200

    Switch build system from Make to Ninja with sanitizing flags
    Enhances build efficiency by utilizing Ninja and adds sanitization flags for improved debugging. This change covers POSIX configurations, enabling thorough testing and ensuring code robustness.

--- make_posix.sh
+++ make_posix.sh
@@ -7,19 +7,19 @@ export forte_bin_dir="bin/posix"
 #rm old folder
 rm -r "./$forte_bin_dir"
 
-#call setup_esp32c3.sh
+#call setup_posix.sh
 ./setup_posix.sh
 
 #goto new Directory
 cd "./$forte_bin_dir"
 
 #make
-make
+ninja -j 24
 
 
 echo "----------------------------------------------------------------------------"
-echo " DONE. to re-make just type:"
+echo " DONE. to re-ninja just type:"
 echo "cd ./$forte_bin_dir"
-echo "make -j" 
+echo "ninja -j 24" 
 echo "----------------------------------------------------------------------------"
 
--- setup_posix.sh
+++ setup_posix.sh
@@ -24,24 +24,46 @@ fi
 
 if [ -d "$forte_bin_dir" ]; then
   
-  echo "For building forte go to $forte_bin_dir and execute \"make\""
+  echo "For building forte go to $forte_bin_dir and execute \"ninja\""
   echo "forte can be found at ${forte_bin_dir}/src"
   echo "forte_tests can be found at ${forte_bin_dir}/tests"
   
   
   cd "./$forte_bin_dir"
   
-  cmake -G "Unix Makefiles" \
+cmake -G "Ninja" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_INSTALL_PREFIX="/home/runner/work/4diac-forte/4diac-forte/install/posix-systemtests" \
+    -DCMAKE_INSTALL_RPATH='$ORIGIN/../lib' \
+    -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
     -DFORTE_ARCHITECTURE=Posix \
+    -DFORTE_EventChainExternalEventListSize=512 \
+    -DFORTE_EventChainEventListSize=1024 \
+    -DFORTE_IO=ON \
+    -DFORTE_LOGLEVEL=LOGDEBUG \
     -DFORTE_COM_ETH=ON \
     -DFORTE_COM_FBDK=ON \
+    -DFORTE_COM_HTTP=ON \
     -DFORTE_COM_LOCAL=ON \
-    -DFORTE_TESTS=OFF \
-    -DFORTE_TESTS_INC_DIRS=${forte_boost_test_inc_dirs} \
-    -DFORTE_TESTS_LINK_DIRS=${forte_boost_test_inc_dirs} \
+    -DFORTE_COM_RAW=ON \
+    -DFORTE_COM_SER=ON \
+    -DFORTE_COM_STRUCT_MEMBER=ON \
+    -DFORTE_IO=ON \
     -DFORTE_MODULE_CONVERT=ON \
     -DFORTE_MODULE_IEC61131=ON \
+    -DFORTE_MODULE_RECONFIGURATION=ON \
+    -DFORTE_MODULE_RT_Events=ON \
+    -DFORTE_MODULE_SIGNALPROCESSING=ON \
     -DFORTE_MODULE_UTILS=ON \
+    -DFORTE_SYSTEM_TESTS=ON \
+    -DFORTE_TESTS=ON \
+    -DFORTE_TESTS_INC_DIRS=${forte_boost_test_inc_dirs} \
+    -DFORTE_TESTS_LINK_DIRS=${forte_boost_test_inc_dirs} \
+    -DFORTE_TEST_SANITIZE=ON \
+    -DCMAKE_CXX_FLAGS="-fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer" \
+    -DCMAKE_C_FLAGS="-fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer" \
+    -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address -fsanitize=undefined" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-fsanitize=address -fsanitize=undefined" \
     ../../
 else
   echo "unable to create ${forte_bin_dir}"
